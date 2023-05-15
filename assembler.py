118d295843913213a67e9c6be493f15f5b7f67f7
aditya <aditya0202sharma@gmail.com>
2023-05-15 17:10:18 +0530

    fixed mov command

--- assembler.py
+++ assembler.py
@@ -1,15 +1,15 @@
 '''
     CSE 112 - Computer Organisation group project.
         group members :
-            Aditya Sharma
+            Aditya Sharma 2022038
             Ayan kumar
-            Aayan hasan
-            Kanishk kumar meena
+            Aayan hasan 2022121
+            Kanishk kumar meena 
 '''
 
 
 # ------------------------------------------taking input throught a text file contating assemble code------------------------------------------------------
-with open('test_case1.txt') as f:  
+with open('t1.txt') as f:  
     code = f.read().splitlines() 
 
 # -----------------------------------------------input code ends---------------------------------------------
@@ -19,8 +19,6 @@ main_lst=[]
 for i in code:
     a=i.split(' ')
     main_lst.append(a)
-le=len(main_lst)
-
 
 # making a dictionary with register 0,1,2,3,4,5,6 mapped to there binary code 
 # there are total 7 general purpose register and one flag register
@@ -40,7 +38,7 @@ operations = {
    "add":["00000","A"], # Performs reg1 = reg2 + reg3. If the computation overflows, then the overflow flag is set and 0 is written in reg1
    "sub":["00001","A"], # Performs reg1 = reg2- reg3. In case reg3 > reg2, 0 is written to reg1 and overflow flag is set.
    "mov1":["00010","B"], # mov immediate
-   "mov2":["00011","C"], # mov immediate but with register
+   "mov2":["00011","C"], # mov with register
    "ld":["00100","D"], # Loads data from mem_addr into reg1.
    "st":["00101","D"], # Stores data from reg1 to mem_addr.
    "mul":["00110","A"], # Performs reg1 = reg2 x reg3. If the computation overflows, then the overflow flag is set and 0 is written in reg1
@@ -155,39 +153,12 @@ def f4():
 
 #******************************************************************************************************************#
 
-# checking error in immediate values
-c=0
-for j in main_lst:
-    c+=1
-    l=len(j)
-    if(j!=['']):
-        dol=j[l-1]
-        if (dol[0]=='$'):
-            n = int(dol[1:])
-            if (n<0 or n>256):
-                print(f"error invalid immidiate value entered {c}.")
+# removing space from the top
+while(main_lst[0]==['']):
+    del main_lst[0]
+le=len(main_lst)
 
-# checking for typos in reg_names and opr_symbols
-c=0
-for j in main_lst:
-    c+=1
-    if(j!=['']):
-        for i in j:
-            if(i[0]=='R'):
-                a=i
-                if(a not in reg):
-                    print(f"error invalid register entered {c}.")
-
-# checking for typos in opr_symbols
-c=0
-for j in main_lst:
-    c+=1
-    if(j!=['']):
-    #var not declred at top how
-        if(j[0] not in opr_sym and (j[0]!='var' and ':' not in j[0])):
-            print(f"error invalid operation symbol is used {c}.")
-
-#*************************************************************************************************************************************
+#**********************************************************************************************************************************
 # adding labels 
 for j in main_lst:
     for i in j:
@@ -203,8 +174,107 @@ for j in main_lst:
             var.append(j[1])
         except:
             print("error variable not declared")
+            error=True
 
 #***********************************************************************************************************************************
+
+# checking error in immediate values
+c=0
+for j in main_lst:
+    c+=1
+    l=len(j)
+    if(j!=['']):
+        dol=j[l-1]
+        if (dol==""):
+            continue
+        else:
+            if(dol[0]=='$'):
+                if (dol[1:].isnumeric()==False):
+                    print(f"error invalid immidiate value entered {c}.")
+                else:
+                    n = int(dol[1:])
+                    if (n<0 or n>256):
+                        print(f"error invalid immidiate value entered {c}.")
+                        error=True
+
+# checking for typos in reg_names
+c=0
+for j in main_lst:
+    c+=1
+    if(j!=['']):
+        for i in j:
+            if(i==''):
+                continue
+            else:
+                if(i[0]=='R'):
+                    a=i
+                    if(a not in reg):
+                        print(f"error invalid register entered {c}.")
+                        error=True
+
+# checking for typos in opr_symbols
+c=0
+for j in main_lst:
+    c+=1
+    if(j!=['']):
+        if(j[0] not in opr_sym and (j[0]!='var' and ':' not in j[0])):
+            print(f"error invalid operation symbol is used {c}.")
+            error=True
+
+#*************************************************************************************************************************************
+
+#checking for correct immediate values
+c=0
+for value in main_lst:
+    operation = value[0]
+    c+=1
+    if (operation=='mov' and value[2][0]!='$' and value[2] in var):
+        print(f"error inncorrect immediate value enetred in line {c}")
+
+    elif (operation in opr_sym and operation!='mov'):
+        # matching the values with op_mnemoics
+        case = operations[operation][1]
+
+        if (case == "B" and value[2][0]!='$' and value[2] in var):
+             print(f"error inncorrect immediate value enetred in line {c}")
+
+        elif (case == "D" and value[2][0]!='$' and value[2] in var):
+            print(f"error inncorrect immediate value enetred in line {c}")
+
+        elif (case == "E" and value[1][0]!='$' and value[1] in var):
+            print(f"error inncorrect immediate value enetred in line {c}")
+
+#*************************************************************************************************************************************
+
+# checking if enought parameters are entered
+c=0
+for value in main_lst:
+    operation = value[0]
+    c+=1
+    if (operation=='mov' and len(value)!=3):
+        print(f"error not enough parameters for mov in line {c}")
+
+    elif (operation in opr_sym and operation!='mov'):
+        # matching the values with op_mnemoics
+        case = operations[operation][1]
+
+        if (case == "B" and len(value)!=3):
+             print(f"error not enough parameters for {operation}")
+
+        elif (case == "A" and len(value)!=4):
+            print(f"error not enough parameters in {operation} in line {c}")
+
+        elif (case == "C" and len(value)!=3):
+            print(f"error not enough parameters in {operation} in line {c}")
+
+        elif (case == "D" and len(value)!=3):
+            print(f"error not enough parameters in {operation} in line {c}")
+
+        elif (case == "E" and len(value)!=2):
+            print(f"error not enough parameters in {operation} in line {c}")
+
+#*************************************************************************************************************************************
+
 
 # checking if all variables are on top
 c_out=0
@@ -220,38 +290,46 @@ for j in range(c_out, le-1):
         c_var+=1
 if (c_var>c_out):
     print("error declare variables at the top")
+    error=True
 
 #************************************************************************************************************************************
 
 # checking for use of undefined variables and use of labels as variables
 c=0
-try:
+if(True):
     for j in main_lst:
         c+=1
         k=len(j)
         if ((j[0]=='ld' or j[0]=='st' or j[0] in lab) and j[k-1] in lab and j[0]!='end:'):
             print(f"error can't use labels as variables in line {c}")
-except:
-    for j in main_lst:
-        c+=1
-        l=len(j)
-        if(j[l-1] not in var and (j[0]=='ld' or j[0]=='st' or j[0] in labels) and j[0]!='end:'):
-            print(f"error undefined variable used in line {c}.")
+            error=True
+    c=0
+    if not error:
+        for j in main_lst:
+            c+=1
+            l=len(j)
+            if(j[0]=='hlt'):
+                break
+            elif(j[l-1] not in var and (j[0]=='ld' or j[0]=='st' or j[0] in labels) and j[0]!='end:'):
+                print(f"error undefined variable used in line {c}.")
+                error=True
 
 #**************************************************************************************************************************************
 
 # checking for undefined labels
 c=0
-try:
+if True:
     for j in main_lst:
         c+=1
         for i in j:
             if(':' in i and i not in labels):
                 print(f"error label is undefined in line {c}")
-except:
-    for j in main_lst:
-        if(':' in j[0] and j[0] in var):
-            print("error can't use varibales as labels")
+                error=True
+    if not error:
+        for j in main_lst:
+            if(':' in j[0] and j[0] in var):
+                print("error can't use varibales as labels")
+                error=True
 
 #**************************************************************************************************************************************
 
@@ -259,7 +337,8 @@ except:
 repeat=[]
 for j in var:
     if (j in repeat):
-        print(f"error repeating variable {j}") 
+        print(f"error repeating variable {j}")
+        error=True 
     else:
         repeat.append(j)
 
@@ -268,6 +347,7 @@ repeat=[]
 for j in labels:
     if(j in repeat):
         print(f"error repeating label {j}")
+        error=True
     else:
         repeat.append(j)
 
@@ -282,10 +362,16 @@ for j in main_lst:
 try:
     if(c==le):
         print("error hlt is missing")
+        error=True
 except:
     if ('hlt' not in main_lst[le-1]):
         print("error hlt not used at end")
+        error=True
 
+# nothing should be after halt
+if ('hlt' not in main_lst[le-1]):
+    print("cant execute lines after hlt")
+    error=True
 
                           # this is printing the binary code part 
 #********************************************************************************************************************************************
@@ -354,7 +440,20 @@ for line in code:
             value.pop(0)
 
         operation = value[0]
-        if operation in opr_sym:
+        if (operation=='mov' and value[2][0]=='$'):
+            a = value[1]
+            b = value[2][1:]
+            b1 = bin(int(b))[2:]
+            s = operations['mov1'][0] + RegAddress[a] + (8 - len(b1)) * "0" + b1
+            print(s)
+
+        elif(operation=='mov' and value[2][0]=='R'):
+            a = value[1]
+            b = value[2]
+            s = operations['mov2'][0] + "00000" + RegAddress[a] + RegAddress[b]
+            print(s)
+
+        elif operation in opr_sym:
             # matching the values with op_mnemoics
             case = operations[operation][1]
 
